10291869fa108956123c0a33a1b8469be3317ed7
chrismuga <chrismuga94@gmail.com>
2019-10-06 17:49:34 +0300

    start tests with pytest

---
+++ operations_test.py
@@ -0,0 +1,5 @@
+from test import addition
+
+
+def test_addition():
+    assert addition(3, 2) == 5
---
+++ test.py
@@ -0,0 +1,6 @@
+def addition(a, b):
+    return a+b
+
+
+sum_value = addition(2, 3)
+print(sum_value)
